1b940a659f47e7d0d7a71af74abd0067e8cd9897
teckthor <ademarjunior@unigranrio.br>
2019-06-07 15:34:17 -0300

    Adicionando ids

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,25 +6,36 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor () {
+  }
+
+  id = 0
 
   todos = [
     {
+      id: this.id += 1,
       name: 'Ademar'
     },
     {
+      id: this.id += 1,
       name: 'Ana clara'
     },
     {
+      id: this.id += 1,
       name: 'Nadir'
     }
 ]
 
   addTodo (newTodoValue) {
+    if (this.todos === []) {
+      id: this.id = 0
+    }
     if (newTodoValue === '') {
-      alert('Campos vazios')
+      alert('O campo de input esta vazio')
     } else {
 
       let newTodo = {
+        id: this.id += 1,
         name: newTodoValue
       }
 
@@ -35,14 +46,12 @@ export class AppComponent {
   deleteTodo (todo) {
     console.log(todo)
     this.todos = this.todos.filter( t => t.name !== todo.name )
+    this.id--
   }
 
   updateTodo(todo) {
     let input = document.querySelector('input')
     input.value = todo.name
-
-    // this.todos
-
 
   }
 
